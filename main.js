4fabda12d9d47e698333762134d41687d0c3af9b
CeylonPark <ceylonp20@gmail.com>
2020-09-23 20:13:54 +0900

    url change

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-const express = require('express');
 const fs = require("fs")
-// 세션용 모듈
-var session = require('express-session');
-var bodyParser = require('body-parser');
-
+const express = require('express');
 const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/static', express.static('public'));
 
-app.get('/login', (request, response) => {
+
+app.get('/auth/login', (request, response) => {
     fs.readFile('index.html', 'utf8', (error, html) => {
         response.send(html);
     });
 });
-
-app.use(bodyParser.urlencoded({extended:false}))
 
 // 세션모듈의 설정
 app.use(session({
@@ -24,7 +22,7 @@ app.use(session({
 }));
 
 // 사용자 페이지, 세션값 유무에 따라서 다른 메세지를 표시
-app.get('/welcome', function(req, res){
+app.get('/', function(req, res){
     if(req.session.displayName){
         res.send(`
             <h2>Hello, ${req.session.displayName} </h2>
@@ -41,11 +39,11 @@ app.get('/welcome', function(req, res){
 // 로그아웃 처리 - 세션 삭제 후 리다이렉트
 app.get('/auth/logout', function(req, res) {
     delete req.session.displayName;
-    res.redirect('/welcome');
+    res.redirect('/');
 });
 
 // 로그인 처리 - 아이디와 패스워드 비교해서 일치하면 세션에 값을 부여하고 리다이렉트
-app.post('/login', (req, res) => {
+app.post('/auth/login', (req, res) => {
     var user = {
         username:'10406',
         password:'qkrwnstkd',
@@ -58,7 +56,7 @@ app.post('/login', (req, res) => {
     //데이터 베이스
     if(uname === user.username && pwd === user.password) {
         req.session.displayName = user.displayName;
-        res.redirect('/welcome');
+        res.redirect('/');
     } else {
         res.send('Who are you? <a href="/auth/login">login</a>');
     }
